11b8310a3da8b60d29950b042ce0ebd732c32c02
Kevin Charter <kevin.charter@acm.org>
2012-02-24 19:59:43 -0700

    Add the skeleton code for converting frames to raw frames.
    This adds to the 'Serialize' module a skeleton of the code to convert
    frames into raw frames. The exported functions are
    
     toRawFrame :: Frame -> RawFrame
    
    and
    
     frameToByteString :: Frame -> ByteString
    
    which uses 'toRawFrame' and 'rawFrameToByteString'. 'toRawFrame' is
    by far the most complex, and splits the task into
    
     - serializing the header
    
     - serializing the flags, if any, and the payload length
    
     - serializing the payload
    
    The complexity will mostly be in serializing the payload for control
    frames, where we'll need to use Builders to encode the details of
    each different type of control frame.

--- Network/SPDY/Serialize.hs
+++ Network/SPDY/Serialize.hs
@@ -3,16 +3,19 @@
 Serialization of SPDY frames to lazy 'ByteString's.
 -}
 
-module Network.SPDY.Serialize (rawFrameToByteString) where
+module Network.SPDY.Serialize
+       (rawFrameToByteString, toRawFrame, frameToByteString) where
 
 import Blaze.ByteString.Builder
 import Data.Bits (setBit, shiftR, (.&.))
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import Data.Monoid
-import Data.Word (Word32)
+import Data.Word (Word8, Word16, Word32)
 
 import Network.SPDY.Frames
+import Network.SPDY.ToWord8
 
 -- | Convert a single raw frame into a strict 'ByteString'.
 rawFrameToByteString :: RawFrame -> B.ByteString
@@ -37,3 +40,78 @@ rawHeaderBuilder header =
       fromWord16be ct
     DataFrameHeader sid ->
       fromWord32be (rawStreamID sid)
+
+-- | Converts a frame into a raw frame.
+toRawFrame :: Frame -> RawFrame
+toRawFrame frame =
+  RawFrame { frameHeader = toRawFrameHeader frame
+           , flagsByte = toFlagsByte frame
+           , payload = toPayload frame }
+
+toRawFrameHeader :: Frame -> RawFrameHeader
+toRawFrameHeader frame =
+  case frame of
+    ControlFrame v d ->
+      ControlFrameHeader v $ toControlType d
+    DataFrame sid _ _ ->
+      DataFrameHeader sid
+
+toControlType :: ControlFrameDetails -> Word16
+toControlType details =
+  case details of
+    SynStream _ _ _ _ _ -> cftSynStream
+    SynReply _ _ _ -> cftSynReply
+    RstStream _ _ -> cftRstStream
+    Settings _ _ -> cftSettings
+    Ping _ -> cftPing
+    GoAway _ _ -> cftPing
+    Headers _ _ _ -> cftHeaders
+    WindowUpdate _ _ -> cftWindowUpdate
+
+toFlagsByte :: Frame -> Word8
+toFlagsByte frame =
+  case frame of
+    ControlFrame _ d ->
+      case d of
+        SynStream f _ _ _ _ -> toWord8 f
+        SynReply f _ _ -> toWord8 f
+        Settings f _ -> toWord8 f
+        Headers f _ _ -> toWord8 f
+        _ -> 0x0
+    DataFrame _ f _ -> toWord8 f
+
+
+toPayload :: Frame -> ByteString
+toPayload frame =
+  case frame of
+    ControlFrame _ d ->
+      toControlPayload d
+    DataFrame _ _ bs ->
+      bs
+
+toControlPayload :: ControlFrameDetails -> ByteString
+toControlPayload = toByteString . toControlPayloadBuilder
+
+toControlPayloadBuilder :: ControlFrameDetails -> Builder
+toControlPayloadBuilder details =
+  case details of
+    SynStream _ id sid pri hb ->
+      error "ni"
+    SynReply _ sid hb ->
+      error "ni"
+    RstStream sid status ->
+      error "ni"
+    Settings _ pairs ->
+      error "ni"
+    Ping pid ->
+      error "ni"
+    GoAway sid status ->
+      error "ni"
+    Headers _ sid hb ->
+      error "ni"
+    WindowUpdate sid dws ->
+      error "ni"
+
+-- | Converts a frame to a 'ByteString'.
+frameToByteString :: Frame -> ByteString
+frameToByteString = rawFrameToByteString . toRawFrame
