450cbcddd6216df51473ddc4199f49c6e1e15b30
Kevin Charter <kevin.charter@acm.org>
2012-03-02 17:32:31 -0700

    Convert raw SYN_STREAM frames into processed frames.
    This adds skeletal code for converting raw frames into processed
    frames, though the only complete implementations are for DATA frames
    and SYN_STREAM frames. In the process, I've added a simple
    'FrameParser' monad that allows strings as errors. This is necessary
    because payload parsing or decompression can fail, and I'd rather
    handler the errors explicitly within this module than throw an
    exception. I may add a special SPDY exception type yet though. I also
    don't attempt to catch the exceptions that could be thrown by the
    zlib-bindings code. I'll leave that for another commit.
    
    At this point, it's high time for some unit tests that check whether
    the conversions from a processed frame to a raw frame and back recover
    the original processed frame, at least for DATA and SYN_STREAM frames.

--- Network/SPDY/Deserialize.hs
+++ Network/SPDY/Deserialize.hs
@@ -1,21 +1,25 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 {-|
 Convert streams of bytes into SPDY frames.
 -}
 module Network.SPDY.Deserialize (rawFrameFromByteString,
                                  parseRawFrame) where
 
+import Blaze.ByteString.Builder
+import Codec.Zlib (Inflate, withInflateInput, flushInflate)
 import Control.Applicative
+import Control.Monad.Error
 import Data.Attoparsec.ByteString (Parser, anyWord8)
 import qualified Data.Attoparsec.ByteString as AP
-import Data.Bits ((.|.), (.&.), shiftL, testBit, clearBit)
+import Data.Bits ((.|.), (.&.), shiftL, shiftR, testBit, clearBit)
 import Data.ByteString (ByteString)
+import Data.IORef (newIORef, modifyIORef, readIORef)
+import Data.Monoid
+
 import Data.Word
 
-import Network.SPDY.Frames (RawFrame(RawFrame),
-                            RawFrameHeader(..),
-                            DataLength(..),
-                            SPDYVersion(..),
-                            StreamID(..))
+import Network.SPDY.Frames
 
 -- | Parse a raw frame from a strict 'ByteString'.
 rawFrameFromByteString :: ByteString -> Either String RawFrame
@@ -66,3 +70,107 @@ netWord32 hi mid1 mid2 lo =
 
 netWord16 :: Word8 -> Word8 -> Word16
 netWord16 hi lo = shiftL (fromIntegral hi) 8 .|. fromIntegral lo
+
+
+-- | Converts a raw frame into the corresponding processed frame,
+-- given a zlib 'Inflate' decompression context.
+toFrame :: Inflate -> RawFrame -> IO Frame
+toFrame inflate raw =
+  either fail return =<< (runErrorT $ runFrameParser $ parseFrame inflate raw)
+
+
+parseFrame :: Inflate -> RawFrame -> FrameParser Frame
+parseFrame inflate rawFrame =
+  case frameHeader rawFrame of
+    ControlFrameHeader v ctype ->
+      fmap (ControlFrame v) $ parseControlFrameDetails inflate ctype flags pl
+    DataFrameHeader sid ->
+      return $ DataFrame sid (DataFlags flags) pl
+    where flags = flagsByte rawFrame
+          pl = payload rawFrame
+
+parseControlFrameDetails :: Inflate -> Word16 -> Word8 -> ByteString
+                            -> FrameParser ControlFrameDetails
+parseControlFrameDetails inflate ctype flags pl
+  | ctype == cftSynStream = do
+      (sid, asid, pri, compressedHeaders) <- parsePayload parseSynStreamContent pl
+      headerBytes <- toByteString <$> (liftIO $ decompress inflate compressedHeaders)
+      headerBlock <- parsePayload parseHeaderBlock headerBytes
+      return $ SynStream (SynStreamFlags flags) sid asid pri headerBlock
+  | ctype == cftSynReply =
+      error "ni"
+  | ctype == cftRstStream =
+      error "ni"
+  | ctype == cftSettings =
+      error "ni"
+  | ctype == cftPing =
+      error "ni"
+  | ctype == cftGoAway =
+      error "ni"
+  | ctype == cftHeaders =
+      error "ni"
+  | ctype == cftWindowUpdate =
+      error "ni"
+  | otherwise =
+      throwError $ "Illegal SPDY frame type '" ++ show ctype ++ "'"
+
+parsePayload :: Parser a -> ByteString -> FrameParser a
+parsePayload p bs =
+  either throwError return $ AP.parseOnly p bs
+
+-- TODO: this is almost identical to 'compress' in the 'Serialize' module. Factor out common code.
+decompress :: Inflate -> ByteString -> IO Builder
+decompress inflate bs = do
+  bref <- newIORef mempty
+  let popper = (maybe (return ()) addBS =<<)
+      addBS dbs = modifyIORef bref (`mappend` fromByteString dbs)
+  withInflateInput inflate bs popper
+  flushInflate inflate
+  readIORef bref
+
+parseSynStreamContent :: Parser (StreamID, Maybe StreamID, Priority, ByteString)
+parseSynStreamContent = do
+  sid <- parseStreamID
+  asid <- parseMaybeStreamID
+  pri <- parsePriority
+  headerBytes <- parseRest
+  return (sid, asid, pri, headerBytes)
+
+parseStreamID :: Parser StreamID
+parseStreamID = StreamID <$> anyWord32
+
+parseMaybeStreamID :: Parser (Maybe StreamID)
+parseMaybeStreamID = nothingIfZero <$> anyWord32
+  where nothingIfZero w | w == 0x0 = Nothing
+                        | otherwise = Just $ StreamID w
+
+parsePriority :: Parser Priority
+parsePriority = (Priority . (`shiftR` 5)) <$> anyWord8
+
+parseHeaderBlock :: Parser HeaderBlock
+parseHeaderBlock = do
+  hc <- parseHeaderCount
+  headerPairs <- replicateM (fromIntegral hc) parsePair
+  return $ HeaderBlock hc headerPairs
+  where parsePair = (,) <$> parseHeaderName <*> parseHeaderValue
+
+parseHeaderCount :: Parser HeaderCount
+parseHeaderCount = HeaderCount <$> anyWord32
+
+parseHeaderName :: Parser HeaderName
+parseHeaderName = HeaderName <$> parseLengthAndBytes
+
+parseHeaderValue :: Parser HeaderValue
+parseHeaderValue = HeaderValue <$> parseLengthAndBytes
+
+parseLengthAndBytes :: Parser ByteString
+parseLengthAndBytes = do
+  len <- fmap fromIntegral $ anyWord32
+  AP.take len
+
+parseRest :: Parser ByteString
+parseRest = AP.takeByteString
+
+newtype FrameParser a =
+  FrameParser { runFrameParser :: ErrorT String IO a }
+  deriving (Functor, Monad, Applicative, MonadError String, MonadIO)
