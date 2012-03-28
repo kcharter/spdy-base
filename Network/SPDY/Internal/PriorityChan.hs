301c4bb449c7e92a24c9767bf527470ccb487561
Kevin Charter <kevin.charter@acm.org>
2012-03-28 16:53:37 -0600

    Add a simple 'priority channel' module, Internal.PriorityChan.
    In order to respond to server-initiated PING frames, we're supposed
    to send a matching PING frame as soon as possible, sending it before
    DATA frames or other control frames that we might have pending. One
    straightforward way is to have a thread for sending frames (or,
    more generally, 'taking actions'), which receives jobs to do on a
    priority queue. PING frames would be given immediate priority.
    
    Since the producer and consumer will be in different threads, and
    since we don't want the consumer to busy-wait when there is nothing
    to do, we have to do something a little fancier than have a priority
    queue in an IORef. I've added a simple module implementing a
    'priority channel'. This is a priority queue in an IORef along with
    a semaphore that has credits only as along as the queue is non-empty.
    The priority queue is the one provided in the 'fingertree' package.
    
    This implementation does not limit the size of the priority queue,
    so if the producer generates jobs very quickly it could strain
    memory resources. In practice, I don't think this is very likely,
    so I'll run with this implementation for now.
    
    The new module isn't used anywhere yet. I'll add some unit tests
    first, and then use it to add PING responses to the Client module.

---
+++ Network/SPDY/Internal/PriorityChan.hs
@@ -0,0 +1,55 @@
+{- | A simple priority channel, essentially a priority queue where the
+producers and consumers are in different threads.
+
+A priority channel is unbounded. Instead of reading elements in FIFO
+order, the consumer reads the lowest-priority element that is
+currently available, blocking if there are no elements.
+
+The interface here is tiny, confined just to uses envisioned in this package.
+
+This module should be imported qualified to avoid name clashes. -}
+
+module Network.SPDY.Internal.PriorityChan (PriorityChan,
+                                           newChan,
+                                           send,
+                                           receive) where
+
+import Control.Concurrent.QSem (QSem, newQSem, signalQSem, waitQSem)
+import Control.Exception (mask_)
+import Data.IORef (IORef, newIORef, atomicModifyIORef)
+
+import qualified Data.PriorityQueue.FingerTree as FT
+
+data PriorityChan p a = PriorityChan {
+  pcSem :: QSem,
+  -- ^ A semaphore that has credits as long as the queue is non-empty.
+  pcIORef :: IORef (FT.PQueue p a)
+  -- ^ The underlying priority queue.
+  }
+
+-- | Creates a new, empty priority channel.
+newChan :: Ord p => IO (PriorityChan p a)
+newChan = do
+  sem <- newQSem 0
+  ref <- newIORef (FT.empty)
+  return PriorityChan { pcSem = sem, pcIORef = ref }
+
+-- | Sends an element with a given priority on the channel. The new
+-- element will appear after all those of lower or equal priority that
+-- have yet to be received by a consumer.
+send :: Ord p => p -> a -> PriorityChan p a -> IO ()
+send p x pq =
+  -- note that we force evaluation of the new priority queue in this
+  -- thread
+  mask_ $ do atomicModifyIORef (pcIORef pq) $ \ftpq ->
+               let ftpq' = FT.add p x ftpq in ftpq' `seq` (ftpq', ())
+             signalQSem (pcSem pq)
+
+-- | Receives the minimum-priority element from the channel, blocking
+-- until one is available.
+receive :: Ord p => PriorityChan p a -> IO a
+receive pq =
+  mask_ (do waitQSem (pcSem pq)
+            atomicModifyIORef (pcIORef pq) $ \ftpq ->
+              maybe (ftpq, Nothing) (\(x, ftpq') -> (ftpq', Just x)) (FT.minView ftpq))
+  >>= maybe (error "Didn't block when receiving on an empty priority channel.") return
