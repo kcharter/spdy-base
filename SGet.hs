b4cb25a933d49c2346d74dd19f004163f67b0d72
Kevin Charter <kevin.charter@acm.org>
2012-04-26 20:00:53 -0600

    Use the Options library in SGet and SPing, allow non-TLS as an option.
    Instead of rolling our own inadequate options processing, use the
    Options library instead. While we're at it, allow these programs to
    optionally turn off TLS. This will help when testing our first attempt
    at a SPDY server.

--- SGet.hs
+++ SGet.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module SGet where
@@ -9,28 +10,37 @@ import Network (PortNumber)
 import System.Environment (getArgs)
 import System.IO (hFlush, stdout, stderr, BufferMode(..), hSetBuffering)
 
+import Options
+
 import Network.SPDY.Client
 import Network.SPDY.Frames (HeaderName(..), HeaderValue(..))
 
+defineOptions "Opts" $ do
+  stringOption "optHost" "host" "localhost" "The host name or IP address of the server."
+  intOption "optPort" "port" 10041 "The server port."
+  stringOption "optPath" "path" "/hello.html" "The path of the resource to get."
+  boolOption "optNoTLS" "no-tls" False "Don't use TLS, just an ordinary unencrypted network connection."
+
 main :: IO ()
-main = do
-  opts <- getOptions
-  c <- client defaultClientOptions
-  let host = optsHost opts
-      port = optsPort opts
-      file = optsFileName opts
-  putStrLn $ "Downloading '" ++ file ++ "' from SPDY server at " ++ host ++ ":" ++ show port
+main = runCommand $ \opts _ -> do
+  let host = optHost opts
+      port = fromIntegral $ optPort opts
+      path = optPath opts
+      useTLS = not $ optNoTLS opts
+  c <- client defaultClientOptions {
+    coptConnectionStyle = if useTLS then CsTLS else CsSocket }
+  putStrLn $ "Downloading '" ++ path ++ "' from SPDY server at " ++ host ++ ":" ++ show port
   -- We're using stdout for output and stderr for frame logging, so we
   -- want to make sure that we're using block buffering on both. This
   -- makes a huge difference.
   let bufferMode = BlockBuffering (Just 4096)
   hSetBuffering stdout bufferMode
   hSetBuffering stderr bufferMode
-  doDownload c host port file
-  where doDownload c host port file = do
+  doDownload c host port useTLS path
+  where doDownload c host port useTLS path = do
           doneHeaders <- newEmptyMVar
           doneData <- newEmptyMVar
-          let cKey = OriginKey (Origin "https" (Host host) port)
+          let cKey = OriginKey (Origin (if useTLS then "https" else "http") (Host host) port)
               -- Apparently, the flip server does not use the
               -- prescribed special header names that start with a
               -- colon. Instead, it expects the headers 'method',
@@ -38,12 +48,12 @@ main = do
               -- with a segmentation fault if only the 'method' and
               -- 'url' headers are present.
               headers = [(HeaderName ":method", HeaderValue "GET"),
-                         (HeaderName ":path", HeaderValue $ fromString $ "/" ++ file),
+                         (HeaderName ":path", HeaderValue $ fromString $ "/" ++ path),
                          (HeaderName ":version", HeaderValue "HTTP/1.1"),
                          (HeaderName ":host", HeaderValue $ fromString $ host ++ ":" ++ show port),
                          (HeaderName ":scheme", HeaderValue "http"),
                          (HeaderName "method", HeaderValue "GET"),
-                         (HeaderName "url", HeaderValue $ fromString $ "http://" ++ host ++ ":" ++ show port ++ "/" ++ file),
+                         (HeaderName "url", HeaderValue $ fromString $ "http://" ++ host ++ ":" ++ show port ++ "/" ++ path),
                          (HeaderName "scheme", HeaderValue "http")]
               opts = defaultStreamOptions {
                 streamOptsHeaderConsumer = \maybeHeaders ->
@@ -66,25 +76,3 @@ main = do
           hFlush stdout
           takeMVar doneHeaders
           takeMVar doneData
-
-getOptions :: IO Options
-getOptions =
-  getHost defaultOptions `fmap` getArgs
-    where getHost opts [] = opts
-          getHost opts (h:r) = getPort (opts { optsHost = h }) r
-          getPort opts [] = opts
-          getPort opts (p:r) = getFileName (opts { optsPort = fromInteger $ read p }) r
-          getFileName opts [] = opts
-          getFileName opts (fn:_) = opts { optsFileName = fn }
-
-data Options = Options {
-  optsHost :: String,
-  optsPort :: PortNumber,
-  optsFileName :: String
-  }
-
-defaultOptions = Options {
-  optsHost = "127.0.0.1",
-  optsPort = fromInteger 10041,
-  optsFileName = "hello.html"
-  }
--- SPing.hs
+++ SPing.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module SPing where
@@ -7,44 +8,32 @@ import Network (PortNumber)
 import System.Environment (getArgs)
 import System.IO (hSetBuffering, BufferMode(..), stdout, stderr)
 
+import Options
+
 import Network.SPDY.Client
 
+defineOptions "Opts" $ do
+  stringOption "optHost" "host" "localhost" "The host name or IP address of the server."
+  intOption "optPort" "port" 10041 "The server port."
+  intOption "optIters" "iters" 10 "The number of pings to send."
+  boolOption "optNoTLS" "no-tls" False "Don't use TLS, just an ordinary unencrypted network connection."
+
 main :: IO ()
-main = do
-  opts <- getOptions
-  c <- client defaultClientOptions
-  let host = optsHost opts
-      port = optsPort opts
-      cKey = OriginKey (Origin "https" (Host host) port)
+main = runCommand $ \opts _ -> do
+  let host = optHost opts
+      port = fromIntegral $ optPort opts
+      useTLS = not $ optNoTLS opts
+      cKey = OriginKey (Origin (if useTLS then "https" else "http") (Host host) port)
+  c <- client $ defaultClientOptions {
+    coptConnectionStyle = if useTLS then CsTLS else CsSocket }
   let bufferMode = BlockBuffering (Just 4096)
   hSetBuffering stdout bufferMode
   hSetBuffering stderr bufferMode
   putStrLn $ "Pinging SPDY server at " ++ host ++ ":" ++ show port
-  replicateM_ (optsIters opts) (doPing c cKey)
+  replicateM_ (optIters opts) (doPing c cKey)
   where doPing c cKey =
           ping defaultPingOptions c cKey >>=
           (putStrLn . \pr -> case pr of
               PingResponse t -> "round-trip time: " ++ show t
               PingTimeout  t -> "timed out after " ++ show t)
 
-getOptions :: IO Options
-getOptions =
-  getHost defaultOptions `fmap` getArgs
-    where getHost opts [] = opts
-          getHost opts (h:r) = getPort (opts { optsHost = h }) r
-          getPort opts [] = opts
-          getPort opts (p:r) = getIters (opts { optsPort = fromInteger $ read p }) r
-          getIters opts [] = opts
-          getIters opts (n:_) = opts { optsIters = read n }
-
-data Options = Options {
-  optsHost :: String,
-  optsPort :: PortNumber,
-  optsIters :: Int
-  }
-
-defaultOptions = Options {
-  optsHost = "127.0.0.1",
-  optsPort = fromInteger 10041,
-  optsIters = 10
-  }
