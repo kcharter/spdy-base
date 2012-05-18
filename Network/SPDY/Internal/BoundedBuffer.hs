0ee0fc57044ff543e6d2f89ed6d3e3d962aa747b
Kevin Charter <kevin.charter@acm.org>
2012-05-18 10:58:33 -0600

    Generalize BoundedBuffer to hold 'sized' values.
    At first I thought that it would be sufficient to bake ByteStrings
    into BoundedBuffer, but I've changed my mind. In order to preserve
    the relative order of headers and data chunks, it would be useful
    to have the buffer hold some kind of stream content type, something
    like
    
      Either HeaderBlock ByteString
    
    For flow control, only the data chunks count, so we'd want a 'size'
    function along the lines of
    
      size :: Either HeaderBlock ByteString -> Int
      size = either (const 0) B.length
    
    where HeaderBlocks are zero-size and ByteStrings have size equal
    to their length.
    
    I've consequently generalized BoundedBuffer by adding a Sized
    type class, and changing the BoundedBuffer type so it can hold
    any type, though most of the functions require a Sized type.

--- Network/SPDY/Internal/BoundedBuffer.hs
+++ Network/SPDY/Internal/BoundedBuffer.hs
@@ -1,15 +1,9 @@
 {- | Bounded buffers.
 
-A bounded buffer is a queue of strict byte strings in which the sum of
-the lengths must be no more than a fixed capacity. These are intended
+A bounded buffer is a queue of sized values in which the sum of
+the sizes must be no more than a fixed capacity. These are intended
 to help implement per-stream flow control and buffering in a SPDY
 endpoint.
-
-The `BoundedBuffer` type could be generalized to any chunk type that
-supports a length operation, but that's more than is necessary here. I
-also suspect that I'm reinventing a wheel that is probably
-better-implemented elsewhere (some kind of bounded channel perhaps)
-but I haven't found a replacement yet.
 
 -}
 
@@ -30,56 +24,63 @@ import Data.IORef (IORef, newIORef, atomicModifyIORef, readIORef)
 import Data.Sequence (Seq, (|>))
 import qualified Data.Sequence as S
 
--- | A FIFO collection of byte strings, bounded by a total capacity.
-data BoundedBuffer =
-  BoundedBuffer { bbChunks :: IORef (Seq ByteString)
+-- | Types with a size.
+class Sized a where
+  size :: a -> Int
+
+instance Sized ByteString where
+  size = B.length
+
+-- | A FIFO collection of sized values, bounded by a total capacity.
+data BoundedBuffer a =
+  BoundedBuffer { bbChunks :: IORef (Seq a)
                 , bbChunkCount :: QSem
                 , bbFreeSpace :: QSemN
                 , bbOriginalCapacity :: Int }
 
 -- | Creates a new empty buffer with a given total capacity.
-new :: Int -> IO BoundedBuffer
+new :: Sized a => Int -> IO (BoundedBuffer a)
 new capacity =
   liftM4 BoundedBuffer (newIORef S.empty) (newQSem 0) (newQSemN capacity) (return capacity)
 
--- | Adds a chunk of data to a buffer. Raises an error if the length
+-- | Adds a chunk to the buffer. Raises an error if the size
 -- of the chunk is greater than the total capacity of the
 -- buffer. Blocks if there is insufficient remaining capacity, until
 -- another thread removes enough chunks to free the required space.
-add :: BoundedBuffer -> ByteString -> IO ()
-add bb bytes = do
+add :: Sized a => BoundedBuffer a -> a -> IO ()
+add bb chunk = do
   when (n > totalCapacity bb)
-    (error ("Can't insert " ++ show n ++
-            " bytes in a buffer with total capacity " ++
+    (error ("Can't insert chunk of size " ++ show n ++
+            " in a buffer with total capacity " ++
             show (totalCapacity bb)))
   waitQSemN (bbFreeSpace bb) n
-  atomicModifyIORef (bbChunks bb) (\chunks -> (chunks |> bytes, ()))
+  atomicModifyIORef (bbChunks bb) (\chunks -> (chunks |> chunk, ()))
   signalQSem (bbChunkCount bb)
-  where n = B.length bytes
+  where n = size chunk
 
 -- | Removes the next available chunk from the buffer, blocking if the
 -- buffer is empty.
-remove :: BoundedBuffer -> IO ByteString
+remove :: Sized a => BoundedBuffer a -> IO a
 remove bb = do
   waitQSem (bbChunkCount bb)
-  bytes <- atomicModifyIORef (bbChunks bb) takeFirst
-  signalQSemN (bbFreeSpace bb) (B.length bytes)
-  return bytes
+  chunk <- atomicModifyIORef (bbChunks bb) takeFirst
+  signalQSemN (bbFreeSpace bb) (size chunk)
+  return chunk
   where takeFirst chunks =
           let (first, rest) = S.splitAt 1 chunks
           in if S.length first > 0
-             then let bytes = S.index first 0
-                  in (rest, bytes)
+             then let chunk = S.index first 0
+                  in (rest, chunk)
              else error "Empty bounded buffer didn't block removal. This shouldn't happen."
 
 -- | The total capacity of the buffer, i.e. the remaining capacity
 -- when the buffer is empty.
-totalCapacity :: BoundedBuffer -> Int
+totalCapacity :: BoundedBuffer a -> Int
 totalCapacity = bbOriginalCapacity
 
 -- | The current remaining capacity of the buffer. This is a snapshot
 -- and may be invalid immediately afterward.
-remainingCapacity :: BoundedBuffer -> IO Int
+remainingCapacity :: Sized a => BoundedBuffer a -> IO Int
 remainingCapacity bb = do
   chunks <- readIORef (bbChunks bb)
-  return $! bbOriginalCapacity bb - F.sum (fmap B.length chunks)
+  return $! bbOriginalCapacity bb - F.sum (fmap size chunks)
